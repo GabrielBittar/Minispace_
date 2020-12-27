34bba2294b5dcfc446b4b606cf95ec85604e4f99
gblinux <gabrielfilosofiaprofessor@gmail.com>
2020-12-27 11:08:53 -0400

    Cria classe background

--- app/src/main/java/com/example/minimalgame/Background.java
+++ app/src/main/java/com/example/minimalgame/Background.java
@@ -1,4 +1,16 @@
 package com.example.minimalgame;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 public class Background {
+
+    int x, y;
+    Bitmap background;
+
+    Background(int screenX, int screenY, Resources res){
+        background = BitmapFactory.decodeResource(res, R.drawable.bg);
+        background = Bitmap.createScaledBitmap(background, screenX, screenY, false);
+    }
 }
