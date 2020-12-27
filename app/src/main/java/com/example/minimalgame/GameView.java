9e5330c2d5803ed1f554c835c873bf9f9ab2f2f5
gblinux <gabrielfilosofiaprofessor@gmail.com>
2020-12-27 15:26:34 -0400

    Cria gameactivity e aprimora background e gameview

--- app/src/main/java/com/example/minimalgame/GameView.java
+++ app/src/main/java/com/example/minimalgame/GameView.java
@@ -1,15 +1,33 @@
 package com.example.minimalgame;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.SurfaceView;
 
 public class GameView extends SurfaceView implements Runnable {
 
     private Thread thread;
     private boolean isPlaying;
+    private int screenX, screenY;
+    private float screenRatioX, screenRatioY;
+    private Paint paint;
+    private Background background1, background2;
 
-    public GameView(Context context) {
+    public GameView(Context context, int screenX, int screenY) {
         super(context);
+
+        this.screenX = screenX;
+        this.screenY = screenY;
+        screenRatioX = 1920f / screenX;
+        screenRatioY = 1080f / screenY;
+
+        background1 = new Background(screenX, screenY, getResources());
+        background2 = new Background(screenX, screenY, getResources());
+
+        background2.x = screenX;
+
+        paint = new Paint();
     }
 
     @Override
@@ -22,11 +40,25 @@ public class GameView extends SurfaceView implements Runnable {
     }
 
     private void update(){
+        background1.x -= 10 * screenRatioX;
+        background2.x -= 10 * screenRatioX;
 
+        if(background1.x + background1.background.getWidth() < 10){
+            background1.x = screenX;
+        }
+        if(background2.x + background2.background.getWidth() < 10){
+            background2.x = screenX;
+        }
     }
 
     private void draw(){
+        if(getHolder().getSurface().isValid()){
+            Canvas canvas = getHolder().lockCanvas();
+            canvas.drawBitmap(background1.background, background1.x, background1.y, paint);
+            canvas.drawBitmap(background2.background, background2.x, background2.y, paint);
 
+            getHolder().unlockCanvasAndPost(canvas);
+        }
     }
 
     private void sleep(){
